042cb0e20ddb0940ede1cf49d203495f68f02f22
Tyler Smith <tyler@technologytherapy.com>
2017-11-16 12:56:43 -0500

    first commit

---
+++ compiler.js
@@ -0,0 +1,18 @@
+import evalJS from './transform'
+
+export default function ({ template, script = 'module.exports={}', styles }) {
+  try {
+    if (script === 'module.exports={}' && !template) throw Error('no data')
+    console.log('SCRIPT', script)
+    const result = evalJS(script)
+    if (template) {
+      result.template = template
+    }
+    return {
+      result: result,
+      styles: styles && styles.join(' ')
+    }
+  } catch (error) {
+    return { error }
+  }
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+  console.log('execute code:', typeof code, code)
+  const result = parser(code)
+
+  /* istanbul ignore next */
+  if (result.error) {
+    console.error(result.error.message)
+    return
+  }
+
+  const compiledCode = compiler(result)
+
+  return compiledCode.result
+  
+}
